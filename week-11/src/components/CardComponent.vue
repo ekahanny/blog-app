63be711649601fa3f2ddbea2f9e2ff802f931774
ekahanny <ekahnny@gmail.com>
2024-04-30 12:57:01 +0800

    finish the homeview and card

--- week-11/src/components/CardComponent.vue
+++ week-11/src/components/CardComponent.vue
@@ -1,24 +1,28 @@
 <template>
   <v-card class="mx-auto" max-width="400" hover>
-    <v-img
-      class="align-end text-white"
-      height="200"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      cover
-    >
-      <v-card-title>Top 10 Australian beaches</v-card-title>
+    <v-img class="align-end text-white" height="200" :src="posts.thumbnail" cover>
+      <v-card-title>{{ posts.title }}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pt-4"> Oct 9, 2019 </v-card-subtitle>
+    <v-card-subtitle class="pt-4">{{ posts.pubDate }}</v-card-subtitle>
 
     <v-card-text>
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <div>{{ posts.description }}</div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" text="Show Details"></v-btn>
+      <v-btn color="orange" text="Show Details" :href="posts.link"></v-btn>
     </v-card-actions>
   </v-card>
 </template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- week-11/src/views/HomeView.vue
+++ week-11/src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
-  <header>
-    <h1 class="p-8 m-8 font-bold text-3xl">Blog System App</h1>
-  </header>
-  <Card />
+  <div>
+    <header>
+      <h1 class="p-8 m-8 font-bold text-3xl">Blog System App</h1>
+    </header>
+    <div v-if="isLoading" class="flex justify-center items-center h-screen">
+      <v-progress-circular color="primary" indeterminate></v-progress-circular>
+    </div>
+    <div class="flex flex-wrap justify-between">
+      <Card v-for="(post, index) in posts" :key="index" :posts="post" class="w-1/2 mb-4" />
+    </div>
+  </div>
 </template>
 
 <script>
 import Card from '../components/CardComponent.vue'
+import axiosInstance from '@/utils/api'
+
 export default {
   components: {
     Card
   },
   data() {
-    return {}
+    return {
+      posts: [],
+      isLoading: false
+    }
+  },
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      try {
+        this.isLoading = true
+        setTimeout(async () => {
+          const response = await axiosInstance.get('kumparan/terbaru')
+          this.posts = response.data.data.posts
+          this.isLoading = false
+        }, 2000)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        this.isLoading = false
+      }
+    }
   }
 }
 </script>
